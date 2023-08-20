3014cef649e7d248721eb777639c6a04c33bed1f
Moro dev <120253206+Moro-tools@users.noreply.github.com>
2023-08-20 22:46:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const button = document.getElementById("whitelist");
-let whitelisted = false
+let whitelisted = false;
+
 button.addEventListener("click", async function () {
   try {
     const response = await fetch('https://api64.ipify.org?format=json');
@@ -7,35 +8,55 @@ button.addEventListener("click", async function () {
     
     const p = document.getElementById("message");
     const container = document.querySelector(".container-whitelisted");
-    
+    const connect = document.getElementById("connect");
+
     let ip = data.ip;
     let p1Text = "Your IP " + ip +" is successfully <span>Whitelisted</span>";
-    connect.href = "fivem://connect/localhost:30120";
-    const p1 = p;
-    p1.innerHTML = p1Text;
-    whitelisted = true
-    container.appendChild(p1);
-    const button = document.getElementById("whitelist");
-    let text = button.innerHTML = '<a href="fivem://connect/16.ip.gl.ply.gg:9998"  id="connect"><img src="fivem.png" height="20px">Connect To Server</a>'
-    const btncon = document.getElementsByClassName("button-container");
-    btncon.appendChild(text) 
+    
+    p.innerHTML = p1Text;
+    connect.href = "fivem://connect/16.ip.gl.ply.gg:9998";
+    whitelisted = true;
+    button.innerHTML = '<a href="fivem://connect/16.ip.gl.ply.gg:9998" id="connect"><img src="fivem.png" height="20px">Connect To Server</a>';
+    button.removeAttribute("href");
   } catch (error) {
     console.error('Error fetching IP:', error);
   }
 });
+
+let menuOpen = false;
+
+const menuIcon = document.getElementById("menu-icon");
+const nav = document.querySelector("nav");
+
+menuIcon.addEventListener("click", () => {
+  if (!menuOpen) {
+    nav.classList.add("active");
+    menuOpen = true;
+  } else {
+    nav.classList.remove("active");
+    menuOpen = false;
+  }
+});
+
+const connect1 = document.getElementById("connect1");
+const p = document.getElementById("message");
+
+connect1.addEventListener("click", function() {
+  if (!whitelisted) {
+    connect1.href = "#"; 
+    p.innerHTML = `Your IP isn't <span class="not-whitelisted">Whitelisted</span>. Please click the button below.`;
+}});
 const connect = document.getElementById("connect");
+
 connect.addEventListener("click", function(){
   if (!whitelisted){
-    const connect = document.getElementById("connect");
-    const p = document.getElementById("message");
     connect.href = "#";
-    let text = p.innerHTML = `Your IP isn't <span class="not-whitelisted">Whitelisted</span> click the button below`
-    text.appendChild(text);
+    p.innerHTML = `Your IP isn't <span class="not-whitelisted">Whitelisted</span>. Please click the button below.`;
   }
-})
-const menuIcon = document.getElementById("menu-icon");
+});
+
+
 menuIcon.addEventListener("click", function(){
-  const menuIcon = document.getElementById("menu-icon");
   const hide = document.getElementById("hide");
   hide.id = "responsive-nav";
   menuIcon.style.display = "none";
@@ -46,7 +67,5 @@ menuIcon.addEventListener("click", function(){
     elem.id = "hide";
     menuX.style.display = "none";
     menuIcon.style.display = "flex";
-  })
-
-  
-})
+  });
+});
